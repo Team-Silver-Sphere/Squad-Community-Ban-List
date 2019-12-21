33f8e40bc039b0d800d7ec7501365a5a4bb6e16b
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-12-21 19:37:57 +0000

    Deployment tweaks

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   apps: [
     {
+      name: 'ban-exporter',
+      script: './ban-exporter/index.js'
+    },
+    {
       name: 'ban-importer',
       script: './ban-importer/index.js'
+    },
+    {
+      name: 'web-server',
+      script: './web-server/index.js'
     }
   ]
 };
