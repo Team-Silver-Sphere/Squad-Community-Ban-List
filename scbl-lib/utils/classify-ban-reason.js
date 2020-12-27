b8868ee8960229d5fccc45e6bfe03be235c663a5
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-12-27 22:57:30 +0000

    Further improve classified ban reasons

--- scbl-lib/utils/classify-ban-reason.js
+++ scbl-lib/utils/classify-ban-reason.js
@@ -6,17 +6,21 @@ const reasons = {
   Teamkilling: /team ?kill|\btk(?:ing|s)?\b/i,
   Trolling: /troll/i,
   Griefing: /grief/i,
-  'Wasting Assets': /destroying assets|wasting (asset|resource)/i,
+  'Wasting Assets': /destroying assets|waste/i,
   Ghosting: /ghosting/i,
 
   Toxic: /flam(e|ing)|harass|insult|toxic/i,
   'Hate Speech': /abusive|bigot|derogatory|discriminat|hate ?spe(e|a)ch|homophobic|nazi|racial|racism|racist|sexism/i,
 
+  'Breaking Seeding Rules': /seeding rules/i,
   Camping: /camping/i,
+  'Current or Recent VAC Ban': /vac ban/i,
   'Helicopter Ramming': /heli ram/i,
   'Locked Squad': /locked squad/i,
+  'No SL Kit': /sl kit/i,
   Recruiting: /recruiting/i,
-  'Soloing Vehicles': /solo/i
+  'Soloing Vehicles': /solo/i,
+  'Stealing Assets': /steal/i
 };
 
 export default function (reason) {
