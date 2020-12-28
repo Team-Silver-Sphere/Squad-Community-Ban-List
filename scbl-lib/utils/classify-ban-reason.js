8bc086ea51abe390c2bd9919d3559f4c6db059fb
Thomas Smyth <thomas.smyth3141@gmail.com>
2020-12-28 00:27:08 +0000

    Further improve classified ban reasons

--- scbl-lib/utils/classify-ban-reason.js
+++ scbl-lib/utils/classify-ban-reason.js
@@ -13,18 +13,22 @@ const reasons = {
   'Hate Speech': /abusive|bigot|derogatory|discriminat|hate ?spe(e|a)ch|homophobic|nazi|racial|racism|racist|sexism|sexist/i,
 
   'AFK': /afk|unassigned/i,
-  'Breaking Seeding Rules': /seeding rules/i,
+  'Breaking Seeding Rules': /seed/i,
   'Breaking Vehicle Priority Rules': /priority/i,
   Camping: /camping/i,
   'Current or Recent VAC Ban': /vac ban/i,
   'Destroying Friendly Assets': /friendly (asset|fob|hab)/i,
-  'Helicopter Ramming': /heli ram/i,
+  'Discussing politics': /politic/i,
+  'Helicopter Ramming': /ramming/i,
+  'Hindering': /hindering/i,
+  'Impersonation': /impersonat/i,
   'Locked Squad': /locked squad/i,
   'No SL Kit': /sl kit/i,
   Recruiting: /recruiting/i,
-  'Soloing Vehicles': /solo|one manning/i,
+  'Soloing Vehicles': /solo|manning/i,
   'Squad Baiting': /baiting|creat(?:ed?|ing)(?: a)? squad|pass sl/i,
-  'Stealing Assets': /steal/i
+  'Stealing Assets': /steal/i,
+  'Streamsniping': /streamsniping/i
 };
 
 export default function (reason) {
