42578ac7868733a9405749d3b6ef6e5575615f14
Thomas Smyth <thomas.smyth3141@gmail.com>
2021-02-13 23:27:12 +0000

    Remove reason tooltip from rows

--- client/src/views/pages/search.js
+++ client/src/views/pages/search.js
@@ -295,22 +295,8 @@ export default function (props) {
                           </AdvancedModal>
                         </td>
                         <td>{edge.node.banList.name}</td>
-                        <td style={{ heigh: 'auto' }}>
+                        <td>
                           {edge.node.reason}
-                          <span id="tooltip-reason-active" data-placement="right">
-                            <i className="ml-2 fa fa-question-circle" />
-                          </span>
-                          <UncontrolledTooltip
-                            boundariesElement="viewport"
-                            data-placement="right"
-                            delay={0}
-                            target="tooltip-reason-active"
-                          >
-                            The ban reasons shown on the Squad Community Ban List are based on
-                            keywords found in the reason and notes supplied by contributing servers.
-                            We cannot guarantee that the reasons displayed reflect the true reason
-                            for the ban. Please see our FAQ for more information.
-                          </UncontrolledTooltip>
                         </td>
                         <td>
                           <BanDates created={edge.node.created} expires={edge.node.expires} />
@@ -417,20 +403,6 @@ export default function (props) {
                         <td>{edge.node.banList.name}</td>
                         <td>
                           {edge.node.reason}
-                          <span id="tooltip-reason-expired" data-placement="right">
-                            <i className="ml-2 fa fa-question-circle" />
-                          </span>
-                          <UncontrolledTooltip
-                            boundariesElement="viewport"
-                            data-placement="right"
-                            delay={0}
-                            target="tooltip-reason-expired"
-                          >
-                            The ban reasons shown on the Squad Community Ban List are based on
-                            keywords found in the reason and notes supplied by contributing servers.
-                            We cannot guarantee that the reasons displayed reflect the true reason
-                            for the ban. Please see our FAQ for more information.
-                          </UncontrolledTooltip>
                         </td>
                         <td>
                           <BanDates created={edge.node.created} expires={edge.node.expires} />
